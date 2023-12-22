b0ed68a07a26a9b1a2f20a840e187efda3a8ad85
Vaibhav Matere <vpm00718897@gmail.com>
2023-12-22 21:15:56 +0530

    productSchema added

---
+++ models/ecommerce/product.models.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+     description: {
+          type: String,
+          required: true,
+     },
+     name: {
+          type: String,
+          required: true,
+     },
+     productImage: {
+          type: String,
+          required: true,
+     },
+     price: {
+          type: Number,
+          required: true,
+          default: 0,
+     },
+     stock: {
+          type: Number,
+          required: true,
+          default: 0,
+     },
+     category: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Category",
+          required: true,
+     },
+     owner: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+     },
+}, { timestamp: true });
+
+export const Product = moongoose.model("Product", productSchema);
